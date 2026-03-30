f63f77e490bde37a234a8c94947367a93ee72a1c
awayatakuma <takuma.awaya@gmail.com>
2026-03-30 17:08:11 +0900

    fix nvim commands

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- User Custom Keymaps
+
+-- Exit insert mode with 'jj'
+map("i", "jj", "<Esc>", { desc = "Exit insert mode" })
+
+-- Line navigation: Start and End of line
+-- These override LazyVim defaults:
+--   <leader>h: Original LazyVim usually maps this to "Health" or "Home"
+--   <leader>l: Original LazyVim usually maps this to "Lazy" (Plugin Manager)
+map("n", "<leader>h", "^", { desc = "Go to beginning of line" })
+map("n", "<leader>l", "$", { desc = "Go to end of line" })
+
+-- GUIDE: Accessing original LazyVim functionality
+-- If you need to access the features previously mapped to <leader>h or <leader>l:
+-- 1. Use the Command Line:
+--    - For Health check: Type ':checkhealth'
+--    - For Lazy UI: Type ':Lazy'
+-- 2. Use the Main Menu (if applicable):
+--    - Press <leader> to see the Which-Key popup and find the alternative keys.
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- User Custom Options
+vim.opt.wrap = true
+vim.opt.list = true
+vim.opt.listchars = { space = "·", tab = "→ ", trail = "·", nbsp = "◊" }
+
+-- Set absolute line numbers
+vim.opt.number = true
+vim.opt.relativenumber = false
