e6fe63d7044dc4eeab94f784b7829fe7ffdcb41d
awayatakuma <takuma.awaya@gmail.com>
2026-03-30 13:55:01 +0900

    remove font install process

--- scripts/setup-brew-install.bash
+++ scripts/setup-brew-install.bash
@@ -5,9 +5,7 @@ source "$(dirname "$0")/envs.bash"
 BREW_FILE="${REPO_DIR}/.config/homebrew/Brewfile"
 
 if ! command -v brew &> /dev/null; then
-    echo "Homebrew is not installed. Please install it first."
     exit 1
 fi
 
-brew tap "yuru7/tap"
 brew bundle install --file="$BREW_FILE"
